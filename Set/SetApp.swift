062ea52fb4287decf5a6bc7b80ee7c3cb1138766
papillon107 <teodormiron75@gmail.com>
2024-03-10 00:48:41 +0200

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Teodor Adrian on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
